04cf1d3176d8bbd4316f2d61b331a46a6f1d75ec
Shane Rowley <srowley@redhat.com>
2022-06-23 18:22:50 -0400

    changed query param to support labels directly

--- src/main/java/io/shaner/PodInfo.java
+++ src/main/java/io/shaner/PodInfo.java
@@ -20,7 +20,10 @@ import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.proto.V1.PodStatus;
+import io.kubernetes.client.proto.V1.PodStatusResult;
 import io.kubernetes.client.util.Config;
 import io.quarkus.logging.Log;
 import io.shaner.dto.Pod;
@@ -43,22 +46,24 @@ public class PodInfo {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Pod> getPodInfo(@QueryParam("key") @DefaultValue("") String key, @QueryParam("value") @DefaultValue("") String value) {
+    public List<Pod> getPodInfo(@QueryParam("labels") @DefaultValue("") String labels) {
         
-        String label = null;
-
-        if (!key.isEmpty() && !value.isEmpty()) {
-            label = key + "=" + value;
+        if (labels.isEmpty()) {
+            labels = null;
         }
 
         try {
             Log.info("Performing pod lookup");
-            V1PodList podList = api.listNamespacedPod(namespace, "true", false, null, null, label, null, null, null, 10, null);
+            V1PodList podList = api.listNamespacedPod(namespace, "true", false, null, null, labels, null, null, null, 10, null);
             Log.debug(podList.toString());
             Log.info("Pods looked up");
             Log.info("Num pods: " + podList.getItems().size());
-            List<Pod> list = podList.getItems().stream().peek(v1Pod -> Log.info(v1Pod.getMetadata().getName())).map(v1Pod -> new Pod(v1Pod.getMetadata().getName(), v1Pod.getStatus().getPodIP())).collect(Collectors.toList());
+            List<Pod> list = podList.getItems().stream()
+                .peek(v1Pod -> Log.info(v1Pod.getMetadata().getName()))
+                .map(v1Pod -> new Pod(v1Pod.getMetadata().getName(), v1Pod.getStatus().getPodIP()))
+                .collect(Collectors.toList());
             Log.info("Pods mapped");
+
             return list;
         }
         catch (ApiException e) {
